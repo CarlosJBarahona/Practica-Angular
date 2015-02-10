fe04298715d874ab2e407155a92432b6ba8ae9e3
Carlos Barahona <ingbarahonacarlos@hotmail.com>
2015-02-10 16:46:28 -0600

    nuevo

--- assets/js/dependencies/funciones.js
+++ assets/js/dependencies/funciones.js
@@ -20,8 +20,6 @@ app.controller('NavbarCtrl', function($scope, $mdSidenav) {
     ];
 });
 
-'use strict';
-
 angular.module('angular-material')
   app.controller('MainCtrl', function ($scope) {
 
@@ -51,24 +49,111 @@ angular.module('angular-material')
 
 });
 
+
+angular.module('angular-material')
+  app.controller('PublicacionCtrl', function ($scope) {
+
+  $scope.posts = [
+   {
+         title:'Post 1 Lorem ipsum dolor',
+         image:'1',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   },
+   {
+         title:'Post 2 Lorem ipsum dolor',
+         image:'2',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   },
+      {
+         title:'Post 3 Lorem ipsum dolor',
+         image:'3',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   },
+      {
+         title:'Post 4 Lorem ipsum dolor',
+         image:'4',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   }
+
+    ];
+
+      $scope.news = [
+   {
+         title:'New 1 Lorem ipsum dolor',
+         image:'1',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   },
+   {
+         title:'New 2 Lorem ipsum dolor',
+         image:'2',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   },
+      {
+         title:'New 3 Lorem ipsum dolor',
+         image:'3',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   },
+      {
+         title:'New 4 Lorem ipsum dolor',
+         image:'4',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   }
+
+    ];
+
+      $scope.events = [
+   {
+         title:'Event 1 Lorem ipsum dolor',
+         image:'1',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   },
+   {
+         title:'Event 2 Lorem ipsum dolor',
+         image:'2',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   },
+      {
+         title:'Event 3 Lorem ipsum dolor',
+         image:'3',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   },
+      {
+         title:'Event 4 Lorem ipsum dolor',
+         image:'4',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   }
+
+    ];
+
+      $scope.series = [
+   {
+         title:'Series 1 Lorem ipsum dolor',
+         image:'1',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   },
+   {
+         title:'Series 2 Lorem ipsum dolor',
+         image:'2',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   },
+      {
+         title:'Series 3 Lorem ipsum dolor',
+         image:'3',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   },
+      {
+         title:'Series 4 Lorem ipsum dolor',
+         image:'4',
+         text:'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa'
+   }
+
+    ];
+
+
+});
+
 angular.module('angular-material')
   app.controller('MenuCtrl', function ($scope) {
 
 });
-
-  
-
-angular.module('progressCircularDemo1', ['ngMaterial'])
-  .controller('AppCtrl', ['$scope', '$interval',
-    function($scope, $interval) {
-      $scope.mode = 'query';
-      $scope.determinateValue = 30;
-      $interval(function() {
-        $scope.determinateValue += 1;
-        if ($scope.determinateValue > 100) {
-          $scope.determinateValue = 30;
-        }
-      }, 100, 0, true);
-    }
-  ]);
 
