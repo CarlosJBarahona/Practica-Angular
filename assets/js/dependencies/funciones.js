46ac91e2778d002922805053dd67d4755c316031
Carlos Barahona <ingbarahonacarlos@hotmail.com>
2015-02-10 12:50:28 -0600

    css

--- assets/js/dependencies/funciones.js
+++ assets/js/dependencies/funciones.js
@@ -55,3 +55,20 @@ angular.module('angular-material')
   app.controller('MenuCtrl', function ($scope) {
 
 });
+
+  
+
+angular.module('progressCircularDemo1', ['ngMaterial'])
+  .controller('AppCtrl', ['$scope', '$interval',
+    function($scope, $interval) {
+      $scope.mode = 'query';
+      $scope.determinateValue = 30;
+      $interval(function() {
+        $scope.determinateValue += 1;
+        if ($scope.determinateValue > 100) {
+          $scope.determinateValue = 30;
+        }
+      }, 100, 0, true);
+    }
+  ]);
+
